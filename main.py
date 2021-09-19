9fa66f3f76f4f4c0fed268a44cc5aba56b05f6ee
Mihail <2big4u7@gmail.com>
2021-09-19 18:12:34 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,13 @@
+a = int(input("Введите переменную A\n"))
+b = int(input("Введите переменную B\n"))
+c = int(input("Введите переменную C\n"))
+D = b**2-4*a*c
+if D>0:
+    x1=(-b+D**0.5)/2*a
+    x2=(-b-D**0.5)/2*a
+    print(x1, x2)
+elif D==0:
+    x1=-b/2*a
+    print(x1)
+else:
+    print("Корней нет")
